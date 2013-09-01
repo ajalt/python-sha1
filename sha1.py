e33a9ec401f0f248f9cc98a5666198de914c0cfe
Jess Austin <jess.austin@gmail.com>
2013-09-01 15:10:00 -0500

    version 3

--- sha1.py
+++ sha1.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
+from __future__ import print_function, unicode_literals
 import struct
+
+try:
+    range = xrange
+except NameError:
+    pass
 
 def _left_rotate(n, b):
     return ((n << b) | (n >> (32 - b))) & 0xffffffff
@@ -27,23 +33,23 @@ def sha1(message):
     original_byte_len = len(message)
     original_bit_len = original_byte_len * 8
     # append the bit '1' to the message
-    message += '\x80'
+    message += b'\x80'
     
     # append 0 <= k < 512 bits '0', so that the resulting message length (in bits)
     #    is congruent to 448 (mod 512)
-    message += '\x00' * ((56 - (original_byte_len + 1) % 64) % 64)
+    message += b'\x00' * ((56 - (original_byte_len + 1) % 64) % 64)
     
     # append length of message (before pre-processing), in bits, as 64-bit big-endian integer
-    message += struct.pack('>Q', original_bit_len)
+    message += struct.pack(b'>Q', original_bit_len)
     # Process the message in successive 512-bit chunks:
     # break message into 512-bit chunks
-    for i in xrange(0, len(message), 64):
+    for i in range(0, len(message), 64):
         w = [0] * 80
         # break chunk into sixteen 32-bit big-endian words w[i]
-        for j in xrange(16):
-            w[j] = struct.unpack('>I', message[i + j*4:i + j*4 + 4])[0]
+        for j in range(16):
+            w[j] = struct.unpack(b'>I', message[i + j*4:i + j*4 + 4])[0]
         # Extend the sixteen 32-bit words into eighty 32-bit words:
-        for j in xrange(16, 80):
+        for j in range(16, 80):
             w[j] = _left_rotate(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1)
     
         # Initialize hash value for this chunk:
@@ -53,7 +59,7 @@ def sha1(message):
         d = h3
         e = h4
     
-        for i in xrange(80):
+        for i in range(80):
             if 0 <= i <= 19:
                 # Use alternative 1 for f from FIPS PB 180-1 to avoid ~
                 f = d ^ (b & (c ^ d))
@@ -106,4 +112,4 @@ if __name__ == '__main__':
         data = args.input
     
     # Show the final digest
-    print 'sha1-digest:', sha1(data)
+    print('sha1-digest:', sha1(data))
